b04ba9f4a655c7137debe44d1c12c0118df84a5c
koluku <korufiru@gmail.com>
2017-05-04 19:10:11 +0900

    Revert "jadeからejsに変更、出力先の変更、各種アップデート"
    This reverts commit 452aabacf4b0e48e74c8621e4add210c096619f5.

--- README.md
+++ README.md
@@ -2,35 +2,88 @@
 
 「Web + Booster」なWebデザインの開発環境を構築します。
 
-## Installation
+# 使い方
 
-以下のコマンドを実行します。
+`git clone`と`npm install`するだけで使えます。Gulpは`gulp`だけで実行できます。
 
-```
-git clone https://github.com/koluku/webooster.git
-npm install
-```
+# ファイル
 
-その後、package.jsonの内容を書き換えてください。
+gulp実行時にhtmlとcssはルート上に出力されますが、jsは移動しません。
 
-## Usage
+## Jade
 
-```
-gulp
-```
+HTMLはJadeで出力します。
 
-```
-gulp release
-```
+### index.jade
 
-## Documents
+基本ファイル。最初は大体これに書いてからモジュール化していく。
 
-- [Files](docs/files.md)
+### _head.jade
 
-## License
+headタグ以下を書くファイル。
+
+### _header.jade
+
+header以下を書くファイル。
+
+### _footer.jade
+
+footer以下を書くファイル。
+
+## Sass
+
+CSSはSassで出力します。
+
+### style.scss
+
+コアファイル。var.scss、_mixin.scss、_base.scss、_layout.scss、_mod.scssをまとめて読み込む。  
+初期では読み込みに関する指定がある。
+
+普段はCSSを書き込まないが、バグ修正などの一時的なコード追加に使う。
+
+### _var.scss
+
+変数ファイル。_mixin.scss、_base.scss、_layout.scss、_mod.scssに影響する変数を書く。  
+初期ではフォントに関する指定がある。
+
+### _mixin.scss
+
+Mixinファイル。_layout.scss、_mod.scssに影響するmixinを書く。  
+初期ではclearfixがある。
+
+### _base.scss
+
+土台ファイル。_mod.scssに影響するファイル。HTMLタグに直接スタイルを指定する。  
+初期ではbody、p、a、imgに指定がある。
+
+### _layout.scss
+
+レイアウトファイル。ページ毎に大きく変動しないスタイルを指定する。  
+初期ではheader、main、footerが未指定状態で存在する。
+
+### _mod.scss
+
+モジュールファイル。classを記述するファイル。  
+初期では何も書かれていない。
+
+### その他
+
+初期でCSSリセットとしてNormalize.cssが読み込まれる。
+
+
+## JavaScript
+
+JavaScriptは初期でjQueryが読み込まれるのでご自由にどうぞ。
+
+### main.js
+
+JavaScriptを記述するためのファイル。  
+初期では何も書かれていない。
+
+### その他
+
+初期でjQueryが読み込まれる。
+
+## ライセンス
 
 [MIT License](LICENSE.md)で公開しています。
-
-## Author
-
-- [koluku](https://github.com/koluku)
--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,49 @@
 var browserSync = require('browser-sync').create();
-var gulp = require('gulp');
-var ejs = require('gulp-ejs');
-var notify = require('gulp-notify');
-var plumber = require('gulp-plumber');
-var pleeease = require('gulp-pleeease');
-var sass = require('gulp-sass');
+var gulp        = require('gulp');
+var jade        = require('gulp-jade');
+var notify      = require('gulp-notify');
+var plumber     = require('gulp-plumber');
+var pleeease    = require('gulp-pleeease');
+var sass        = require('gulp-sass');
 
-var paths = {
-  ejs: './src/ejs/',
-  scss: './src/scss/'
+paths = {
+  jade: ['./jade/'],
+  scss: ['./scss/']
 };
 
 gulp.task('browser-sync', function() {
    browserSync.init({
      server: {
-       baseDir: './src/'
+       baseDir: './'
      }
    });
 });
-gulp.task('ejs:develop', function() {
-  gulp.src([paths.ejs + '**/!(_)*.ejs'])
+gulp.task('jade', function() {
+  gulp.src([paths.jade + '**/!(_)*.jade'])
     .pipe(plumber({
       errorHandler: notify.onError('Error: <%= error.message %>')
     }))
-    .pipe(ejs('',{'ext': '.html'}))
-    .pipe(gulp.dest('./src/'))
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./'))
     .pipe(browserSync.stream());
 });
-gulp.task('ejs:release', function() {
-  gulp.src([paths.ejs + '**/!(_)*.ejs'])
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
-    }))
-    .pipe(ejs('',{'ext': '.html'}))
-    .pipe(gulp.dest('./build/'));
-});
-gulp.task('sass:develop', function() {
+gulp.task('sass', function() {
   gulp.src(paths.scss + '**/*.scss')
     .pipe(plumber({
       errorHandler: notify.onError('Error: <%= error.message %>')
     }))
-    .pipe(sass())
-    .pipe(pleeease())
-    .pipe(gulp.dest('./src/'))
-    .pipe(browserSync.stream());
-});
-gulp.task('sass:release', function() {
-  gulp.src(paths.scss + '**/*.scss')
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
-    }))
-    .pipe(sass())
+    .pipe(sass({outputStyle: 'compressed'}))
     .pipe(pleeease({
       browsers: ['last 2 versions'],
-      minifier: true
+      minifier: false
     }))
-    .pipe(gulp.dest('./build/'));
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['ejs:develop','sass:develop','browser-sync'], function() {
-  gulp.watch([paths.ejs + '**/*.ejs'], ['ejs:develop']);
-  gulp.watch([paths.scss + '**/*.scss'], ['sass:develop']);
+gulp.task('default', ['jade','sass','browser-sync'], function() {
+  gulp.watch([paths.jade + '**/*.jade'], ['jade']);
+  gulp.watch([paths.scss + '**/*.scss'], ['sass']);
 });
-gulp.task('release', ['ejs:release','sass:release']);
